6c89e0cc4cf6d10d5cd43018d800ea23134c56a1
johnn <johnnyzaev@gmail.com>
2024-01-31 17:47:55 +0400

    Start button now works

--- Assets/Game/Runtime/Services/SceneDataService.cs
+++ Assets/Game/Runtime/Services/SceneDataService.cs
@@ -1,9 +1,13 @@
+using TMPro;
 using UnityEngine;
 
 namespace Game.Runtime.Services
 {
     public class SceneDataService : MonoBehaviour
     {
-        
+        [field: SerializeField] public Canvas MainMenuCanvas { get; private set; }
+        [field: SerializeField] public Canvas GameCanvas { get; private set; }
+        [field: SerializeField] public TMP_Text GameCanvasText { get; private set; }
+        public bool GameStarted { get; set; }
     }
 }
--- Assets/Game/Runtime/Systems/MainMenuInputSystem.cs
+++ Assets/Game/Runtime/Systems/MainMenuInputSystem.cs
@@ -1,16 +1,24 @@
+using DG.Tweening;
+using Game.Runtime.Services;
 using Game.Runtime.Utils;
+using Leopotam.EcsLite.Di;
 using Leopotam.EcsLite.Unity.Ugui;
 using UnityEngine;
 using UnityEngine.Scripting;
 
-namespace Game.Runtime.Systems {
-    internal sealed class MainMenuInputSystem : EcsUguiCallbackSystem 
+namespace Game.Runtime.Systems 
+{
+    internal sealed class MainMenuInputSystem : EcsUguiCallbackSystem
     {
+        private EcsCustomInject<SceneDataService> _sceneDataService;
+        
         [Preserve]
         [EcsUguiClickEvent(Idents.Ui.Play, Idents.Worlds.Events)]
         private void OnClickStart(in EcsUguiClickEvent e)
         {
-            Debug.Log("Start");
+            _sceneDataService.Value.MainMenuCanvas.gameObject.SetActive(false);
+            _sceneDataService.Value.GameCanvas.gameObject.SetActive(true);
+            _sceneDataService.Value.GameCanvasText.transform.DOScale(Vector3.one * 1.5f, 1.5f).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InOutBack);
         }
         
         [Preserve]
